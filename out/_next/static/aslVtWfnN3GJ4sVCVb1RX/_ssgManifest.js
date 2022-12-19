f3e6dacc0721a8abb447501c2cb8664ef375c7a3
Austin Mattix <81455525+AustinMattix23712@users.noreply.github.com>
2022-12-19 08:22:51 -0500

    Delete out directory

--- out/_next/static/aslVtWfnN3GJ4sVCVb1RX/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Ffixtures\u002Faside","\u002Ffixtures\u002Fscrollable","\u002Ffixtures\u002Fsimple","\u002Ffixtures\u002Fsticky"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
